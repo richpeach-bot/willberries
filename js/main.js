d596e2352cac40166d71b52dcbc2f7468672ffd3
Yana Trushnikova <58902064+richpeach-bot@users.noreply.github.com>
2023-08-22 02:01:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
